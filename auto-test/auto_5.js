9a367c25b756f7255dd0e051613aea6d213fb915
Ivan <lorf1991@gmail.com>
2021-07-30 17:18:48 +0300

    comlited auto test module 5

--- auto-test/auto_5.js
+++ auto-test/auto_5.js
@@ -476,9 +476,11 @@ function runTask_18() {
       SUPERUSER: "superuser",
     };
 
+    accessLevel;
+
     constructor({ email, accessLevel }) {
       super(email);
-      this.accessLevel = Admin.AccessLevel.SUPERUSER;
+      this.accessLevel = accessLevel;
     }
 
     // Пиши код выше этой строки
@@ -492,3 +494,65 @@ function runTask_18() {
   console.log(mango.email); // mango@mail.com
   console.log(mango.accessLevel); // superuser
 }
+
+// runTask_18();
+
+//============== 19/19 ==================
+
+function runTask_19() {
+  class User {
+    email;
+
+    constructor(email) {
+      this.email = email;
+    }
+
+    get email() {
+      return this.email;
+    }
+
+    set email(newEmail) {
+      this.email = newEmail;
+    }
+  }
+  class Admin extends User {
+    // Пиши код ниже этой строки
+
+    static AccessLevel = {
+      BASIC: "basic",
+      SUPERUSER: "superuser",
+    };
+
+    accessLevel;
+    blacklistedEmails = [];
+
+    constructor({ email, accessLevel }) {
+      super(email);
+      this.accessLevel = accessLevel;
+    }
+
+    isBlacklisted(email) {
+      return this.blacklistedEmails.includes(email);
+    }
+
+    blacklist(email) {
+      this.blacklistedEmails.push(email);
+    }
+
+    // Пиши код выше этой строки
+  }
+
+  const mango = new Admin({
+    email: "mango@mail.com",
+    accessLevel: Admin.AccessLevel.SUPERUSER,
+  });
+
+  console.log(mango.email); // mango@mail.com
+  console.log(mango.accessLevel); // superuser
+  mango.blacklist("poly@mail.com");
+  console.log(mango.blacklistedEmails); // 'poly@mail.com'
+  console.log(mango.isBlacklisted("mango@mail.com")); //  false
+  console.log(mango.isBlacklisted("poly@mail.com")); // true
+}
+
+// runTask_19();
